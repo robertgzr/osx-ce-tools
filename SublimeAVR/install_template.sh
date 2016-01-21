fd7dacc7ca787b08360d05917ec3044657b37f81
Robert G <robert.guenzler@student.htw-berlin.de>
2016-01-21 15:57:30 +0100

    better error handling

--- SublimeAVR/install_template.sh
+++ SublimeAVR/install_template.sh
@@ -12,31 +12,42 @@ ZIP="htw.zip"
 
 SYS=$(uname)
 
-if [ -d "$DIR" ]; then
-    echo "Zipping $DIR..."
-    zip -r $ZIP $DIR
-fi
+echo "SublimeAVR - htw template installer:"
 
-# Mac
-if [ $SYS = "Darwin" ]
-then
-    echo "Detected OS X"
-    if [ -d "$OSXPATH" ]; then
-        cp -iv "$ZIP" "$OSXPATH"
+function zipper
+{
+    if [ -d "$DIR" ]; then
+        echo ">>> Creating $ZIP..."
+        which zip >/dev/null && zip -r $ZIP "$DIR/*" && return 0 || echo ">>> [!] zip is not installed" && return 1
     else
-        echo "SublimeAVR not found"
+        echo ">>> [!] Missing template files"
+        return 1
     fi
-    echo "Success!"
-# Linux
-elif [ $SYS = "Linux" ]
-then
-    echo "Detected Linux"
-    if [ -d "$LINUXPATH" ]; then
-        cp -iv "$ZIP" "$LINUXPATH"
+}
+
+function mover
+{
+    # Mac
+    if [ $SYS = "Darwin" ]
+    then
+        echo ">>> detected OS: OS X"
+        if [ -d "$OSXPATH" ]; then
+            cp -iv "$ZIP" "$OSXPATH" && echo ">>> Success!"
+        else
+            echo ">>> [!] SublimeAVR not found"
+        fi
+    # Linux
+    elif [ $SYS = "Linux" ]
+    then
+        echo ">>> detected OS: Linux"
+        if [ -d "$LINUXPATH" ]; then
+            cp -iv "$ZIP" "$LINUXPATH" && echo ">>> Success!"
+        else
+            echo ">>> [!] SublimeAVR not found"
+        fi
     else
-        echo "SublimeAVR not found"
+        echo ">>> [!] Sorry $(uname) not supported"
     fi
-    echo "Success!"
-else
-    echo "Sorry $(uname) not supported"
-fi
+}
+
+zipper && mover
